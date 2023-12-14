00467234f3934362e69f95b32826c9c14c0406e7
xiangn <xiangnrodriguez@gmail.com>
2023-12-14 18:03:02 -0400

    First Commit

---
+++ src/main/java/ve/ucla/informationsystems/InformationSystemsApplication.java
@@ -0,0 +1,13 @@
+package ve.ucla.informationsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ve/ucla/informationsystems/InformationSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package ve.ucla.informationsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
